ece848248fa540e796b26e3e3bc65ff082268f13
jpschew <jps.chew@gmail.com>
2022-03-30 00:15:46 +0800

    added code to calculate a side of triangle, modified test code

--- triangleCalculator.go
+++ triangleCalculator.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func convertDegtoRad(angle float64) float64 {
+	// 2 pi = 360 deg
+	// 1 deg = 2pi/360
+	return angle * (math.Pi / 180)
+}
 func calculateSide(length1, length2, angle float64) float64 {
 
 	//Insert the code here
+	angle = convertDegtoRad(angle)
+	length3 := math.Pow(math.Pow(length1, 2)+math.Pow(length2, 2)-2*length1*length2*math.Cos(angle), 0.5)
 
 	//Do not remove this line
 	fmt.Println("The 3rd length of the triange is", length3)
