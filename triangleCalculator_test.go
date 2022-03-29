ddf88234cf93f603fe0dc8d700fce0afdd8160b2
jpschew <jps.chew@gmail.com>
2022-03-30 00:16:30 +0800

    modified test code

--- triangleCalculator_test.go
+++ triangleCalculator_test.go
@@ -3,8 +3,11 @@ package main
 import "testing"
 
 func TestTriangleCalculator(t *testing.T) {
-	get := calculateSide(10, 10, 45)
-	res := 9.74349024921019
+
+	// get := calculateSide(10, 10, 45)
+	// res := 9.74349024921019 // answer is wrong shd be 7.653668647301795
+	get := calculateSide(3, 4, 90)
+	res := 5.0
 	if get != res {
 		t.Errorf("CalculateSide() = %f Want %f Got %f", get, res, get)
 	}
